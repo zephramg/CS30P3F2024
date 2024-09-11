e63b26951c2edfccdf1a96ad4e0c4b7105ed453d
38020001 <38020001@L815WD220006.edu.cbe.ab.ca>
2024-09-11 13:38:31 -0600

    13:38

---
+++ chapter10/src/skillbuilders/SemAvg.java
@@ -0,0 +1,86 @@
+package skillbuilders;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class SemAvg {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SemAvg window = new SemAvg();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public SemAvg() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 539, 357);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Grade #1:");
+		lblNewLabel.setBounds(31, 33, 63, 56);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblGrade = new JLabel("Grade #2:");
+		lblGrade.setBounds(31, 71, 63, 56);
+		frame.getContentPane().add(lblGrade);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Grade #3:");
+		lblNewLabel_1_1.setBounds(31, 109, 63, 56);
+		frame.getContentPane().add(lblNewLabel_1_1);
+		
+		textField = new JTextField();
+		textField.setBounds(89, 51, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(89, 89, 86, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(89, 127, 86, 20);
+		frame.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Calculate Average");
+		btnNewButton.setBounds(89, 176, 126, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setBounds(284, 33, 170, 249);
+		frame.getContentPane().add(lblNewLabel_1);
+	}
+}
