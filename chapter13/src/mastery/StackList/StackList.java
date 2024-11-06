28e27c4dc76a4bae2266803a0a3e358e8fbb955e
38020001 <38020001@L815WD220006.edu.cbe.ab.ca>
2024-11-06 13:10:14 -0700

    13:10

---
+++ chapter13/src/mastery/StackList/StackList.java
@@ -0,0 +1,50 @@
+package mastery.StackList;
+
+public class StackList {
+    private Node top;
+
+    // Inner Node class to represent each element in the stack
+    private class Node {
+        Object data;
+        Node next;
+
+        Node(Object data) {
+            this.data = data;
+        }
+    }
+
+    // Constructor for an empty stack
+    public StackList() {
+        top = null;
+    }
+
+    // Push operation to add an element to the top of the stack
+    public void push(Object data) {
+        Node newNode = new Node(data);
+        newNode.next = top;
+        top = newNode;
+    }
+
+    // Pop operation to remove the top element from the stack
+    public Object pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        Object data = top.data;
+        top = top.next;
+        return data;
+    }
+
+    // Peek operation to view the top element without removing it
+    public Object peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return top.data;
+    }
+
+    // Check if the stack is empty
+    public boolean isEmpty() {
+        return top == null;
+    }
+}
---
+++ chapter13/src/mastery/StackList/StackListDemo.java
@@ -0,0 +1,37 @@
+package mastery.StackList;
+
+public class StackListDemo {
+    public static void main(String[] args) {
+        StackList stack = new StackList();
+
+        // Pushing elements onto the stack
+        System.out.println("Pushing elements: 10, 20, 30");
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        // Peeking at the top element
+        System.out.println("Top element (peek): " + stack.peek());
+
+        // Popping elements from the stack
+        System.out.println("\n" + "Popping elements:");
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+
+        // Trying to peek or pop from an empty stack
+        try {
+            System.out.println("\n" + "Attempting to peek on an empty stack:");
+            System.out.println(stack.peek());
+        } catch (IllegalStateException e) {
+            System.out.println("Caught exception: " + e.getMessage());
+        }
+
+        try {
+            System.out.println("\n" + "Attempting to pop from an empty stack:");
+            stack.pop();
+        } catch (IllegalStateException e) {
+            System.out.println("Caught exception: " + e.getMessage());
+        }
+    }
+}
