17d42e12e6a414f86f5015b6c5b5c137ab1bdf90
38020001 <38020001@L815WD220006.edu.cbe.ab.ca>
2024-11-26 15:22:59 -0700

    15:22

--- phidgetsRover/src/challenge/challenge.java
+++ phidgetsRover/src/challenge/challenge.java
@@ -1,7 +1,8 @@
 /* Challenge: Make the rover travel approx 1m, turn 90 degrees, 
 drive forward another 1m, turn 90, etc., such that it drives in 
-one full square and ends in the same position as it started. */ 
-  
+one full square and ends in the same position as it started, 
+with an adjustment to straighten at the end. */
+
 package challenge;
 
 // Add Phidgets Library
@@ -27,7 +28,8 @@ public class challenge {
 
         // Define the time to drive forward 1m (adjust based on rover speed)
         int driveTime = 962; // Approximate time for 1 meter at full speed
-        int turnTime = 296; // Approximate time for a 90° turn
+        int turnTime = 310; // Approximate time for a 90° turn
+        int finalTurnAdjustment = 72; // Additional time to straighten out at the end
 
         // Repeat for four sides of the square
         for (int i = 0; i < 4; i++) {
@@ -41,10 +43,18 @@ public class challenge {
             rightMotors.setTargetVelocity(0);
             Thread.sleep(500);
 
-            // Turn 90 degrees
-            leftMotors.setTargetVelocity(1);
-            rightMotors.setTargetVelocity(-1);
-            Thread.sleep(turnTime);
+            // Turn 90 degrees (adjust last turn slightly)
+            if (i == 3) {
+                // Last turn includes slight adjustment
+                leftMotors.setTargetVelocity(1);
+                rightMotors.setTargetVelocity(-1);
+                Thread.sleep(turnTime + finalTurnAdjustment);
+            } else {
+                // Standard 90° turn
+                leftMotors.setTargetVelocity(1);
+                rightMotors.setTargetVelocity(-1);
+                Thread.sleep(turnTime);
+            }
 
             // Stop motors briefly
             leftMotors.setTargetVelocity(0);
